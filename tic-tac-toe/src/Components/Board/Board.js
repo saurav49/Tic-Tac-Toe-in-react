05c75f8694565907850445a8d808993872a50b4a
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2021-01-19 14:18:10 +0600

    chore - completed the app

--- tic-tac-toe/src/Components/Board/Board.js
+++ tic-tac-toe/src/Components/Board/Board.js
@@ -3,21 +3,18 @@ import Square from "../Square/Square";
 
 // Board
 const Board = () => {
-    //state
     // BoardState
-    const [boardSquare, setBoardSquare] = useState([Array(9).fill(null)]);
+    const [boardSquare, setBoardSquare] = useState(Array(9).fill(null));
     // TurnState
     const [isXturn, setIsXturn] = useState(true);
 
   // Create a handleClick Function
   const handleClick = (index) => {
       // mutate the state 
-      let newBoard = [...boardSquare]
+      const newBoard = [...boardSquare];
 
       // if value is already there then return
-      if(newBoard[index]) {
-          return
-      }
+      if(winner || newBoard[index]) return;
       // put x or o in it
       newBoard[index] = isXturn ? "X" : "O";
       // setstate the board state
@@ -31,14 +28,48 @@ const Board = () => {
           <Square value={boardSquare[index]} onclick = {() => handleClick(index)}/>
       )
   }
+
+  const calculateWinner = (board) => {
+      const winningCombo = [
+          [0, 1, 2],
+          [3, 4, 5], 
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 7],
+          [2, 4, 6]
+      ]
+
+      for(let index=0; index<winningCombo.length; index++) {
+          const [a, b, c] = winningCombo[index];
+
+          console.log(`a : ${board[a]}, b : ${board[b]}, c : ${board[c]}`);
+          if(board[a] && board[a] === board[b] && board[a] === board[c]) {
+            return board[a];
+          }
+        }
+        return null;
+  }
+
+  let winner = calculateWinner(boardSquare);
+  console.log(winner);
+  let whoseTurn = winner ? `Winner is ${winner}` : `Next Player : ${isXturn ? "X" : "O"}`;
   
   return(
-    <div>
-        <div></div>
-        <div>{renderSquare(0)}{renderSquare(1)}{renderSquare(2)}</div>
-        <div>{renderSquare(3)}{renderSquare(4)}{renderSquare(5)}</div>
-        <div>{renderSquare(6)}{renderSquare(7)}{renderSquare(8)}</div>
-    </div>
+      <React.Fragment>
+        <div className="board">
+            <div className="status">
+                {whoseTurn}
+            </div>
+            <div className="boardRow">{renderSquare(0)}{renderSquare(1)}{renderSquare(2)}</div>
+            <div className="boardRow">{renderSquare(3)}{renderSquare(4)}{renderSquare(5)}</div>
+            <div className="boardRow">{renderSquare(6)}{renderSquare(7)}{renderSquare(8)}</div>
+        </div>
+        <div className="circle1"></div>
+        <div className="circle2"></div>
+    </React.Fragment>
+
   )
 }
 
--- tic-tac-toe/src/Components/Square/Square.js
+++ tic-tac-toe/src/Components/Square/Square.js
@@ -2,8 +2,10 @@ import React from "react";
 import "../../App.css";
 
 const Square = ({value, onclick}) => {
+    let styleValue = value ===  "X" ? "styleX" : "styleO";
+
   return(
-    <button className="square" onClick={onclick}>{value}</button> 
+    <button className= {`${"square"} ${styleValue}`} onClick={onclick}>{value}</button> 
   )  
 } 
 
